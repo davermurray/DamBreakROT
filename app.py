723771f0c2131c9f1f2f1859a64e13cb447ea027
davermurray <59978972+davermurray@users.noreply.github.com>
2025-07-14 20:51:02 +0000

    TT + eq update

--- app.py
+++ app.py
@@ -52,6 +52,15 @@ def dam_external_risk(id=None):
         inventory = None
     return inventory
 
+def highlight_by_damtype(value):
+    if dam_type == "Earthen" and value == "Froehlich":
+        color = f"background-color: lightgreen;"
+    elif dam_type != "Earthen" and value == "SMPDBK":
+        color = f"background-color: lightgreen;"
+    else:
+        color = None
+    return  color
+
 st.markdown("""
         <style>
                .block-container {
@@ -72,7 +81,7 @@ Dv_acft_default = 1000.0
 fedId_default = None
 dam_type_order = ["Earthen", "Concrete Gravity", "Concrete Arch"]
 
-dam_input = st.text_input("Dam NID Search:",value="")
+dam_input = st.text_input("Dam Name / NID Id Search:",value="")
 
 dam_suggests, search_exception = dam_get(dam_input)
 if search_exception:
@@ -85,12 +94,14 @@ if isinstance(dam_suggests, dict) and "dams" in dam_suggests.keys() and dam_sugg
     selection_mode="single-row") 
     if len(damdf_select.selection.rows) > 0:
         fedId_default = dam_suggestion_df.iloc[damdf_select.selection.rows,[3]].values[0][0]
-
+    else:
+        fedId_default = dam_suggestion_df.iloc[0,[3]].values[0]
+        st.write('Using First Row', fedId_default)
 #Set the federal Id - manually or through the search above as default
-fedId = st.text_input("NID Id",value=fedId_default)
+#fedId = st.text_input("NID Id",value=fedId_default)
 
-if fedId != None: 
-    dam_query, exception_msg = dam_inventory(fedId)
+if fedId_default != None: 
+    dam_query, exception_msg = dam_inventory(fedId_default)
     if exception_msg:
         st.caption(exception_msg)
   
@@ -119,21 +130,26 @@ if fedId != None:
             dam_type_order = ["Concrete Gravity", "Concrete Arch", "Earthen"]
     except:
         st.write('No Dam found.')  
-        
+
+    st.caption('Initial Breach Head is set to NID Dam Height, and Reservoir Volume is set to Max Volume. Click on columns in displayed dataframe to use their values instead. Manually edit info in table as needed.')
+# Inputs 
+#         
 col1, col2 = st.columns(2)
 
 with col1:
     dam_type = st.selectbox("Dam Type", dam_type_order)
-    Dh_ft = st.number_input("Breach Head (ft)", min_value=0.0, value=Dh_ft_default, step=1.0)    
+    Dh_ft = st.number_input("Breach Head (ft)", min_value=0.0, value=Dh_ft_default, step=1.0)
+    tw_width = st.number_input("Max Tailwater Width (ft) - Arch Dams only ", min_value=0.0, value=100.0, step=1.0)    
 
 with col2:
     failure_mode = st.selectbox("Failure Mode", ["Overtopping","Piping"])
     Dv_acft = st.number_input("Reservoir Volume (acre-ft)", min_value=0.0, value=Dv_acft_default, step=10.0)
+    erodability = st.selectbox("Dam Erodability - Von Thun Only", ["Erosion Resistant","Easily Erodible"])
 
 downstream_mileage = st.number_input("Downstream Point of Interest (mi)", min_value=0.0, value=10.0, step=0.1)
 
 # Constants and conversions
-Dv_m3 = Dv_acft * 1233.48  # Convert to cubic meters
+Dv_m3 = Dv_acft * 1233.48  # Convert to cubic meters - Original rules of thumb uses 1233
 Dh_m = Dh_ft * 0.3048  # Convert to meters
 
 # Froehlich breach width
@@ -141,17 +157,35 @@ K = 1.0 if failure_mode == "Piping" else 1.4
 Bf = 3.28 * (0.1803 * (K * (Dv_m3 ** 0.32) * (Dh_m ** 0.19)))  # in feet
 
 # Von Thun & Gillette breach width
-Bv = 1.5 * Dh_ft  # in feet
+#Bv = 1.5 * Dh_ft  # in feet
+if Dv_m3 < 1233000:
+    bw_const = 6.1
+elif (Dv_m3 < 6165000) and (Dv_m3 >= 1233000):
+    bw_const = 18.3
+elif (Dv_m3 < 12330000) and (Dv_m3 >= 6165000):
+    bw_const = 42.7
+elif Dv_m3 >= 12330000:
+    bw_const = 54.9
 
-# MacDonald & Langridge-Monopolis breach width (example formula)
-dam_type_bw = {"Earthen":3, "Concrete Gravity":5, "Concrete Arch":0.9}
+Bv = round((2.5 * (Dh_m) + bw_const)*3.28,2)
+
+# MacDonald & Langridge-Monopolis breach width and timing
+dam_type_bw = {"Earthen":[3,10], "Concrete Gravity":[5,40], "Concrete Arch":[0.9,50]}
 #Bm = (0.5 * math.log10(Dv_m3)) + (0.6 * Dh_ft) + 0.5  # in feet
-Bm = dam_type_bw[dam_type] * Dh_ft
+if dam_type == "Concrete Arch":
+    Bm = dam_type_bw[dam_type][0] * tw_width
+else:
+    Bm = dam_type_bw[dam_type][0] * Dh_ft
 
 # Breach Formation Time (converted to minutes)
 Tf_froehlich = 0.00254 * (Dv_m3 ** 0.53) * (Dh_m ** -0.90) * 60
-Tf_von_thun = 0.0178 * (Dh_m ** 1.4) * 60
-Tf_smpdbk = 0.0179 * (Dv_m3 ** 0.37) * 60
+
+if erodability == "Erosion Resistant":
+    Tf_von_thun = (0.02 * (Dh_m) + 0.25)* 60
+else:
+    Tf_von_thun = 0.015 * Dh_m * 60
+
+Tf_smpdbk = Dh_ft / dam_type_bw[dam_type][1] 
 
 # Peak outflow estimates
 Qp_froehlich = 0.607 * (Dv_m3 ** 0.295) * (Dh_m ** 1.24)       # m³/s
@@ -159,14 +193,14 @@ Qp_froehlich = 0.607 * (Dv_m3 ** 0.295) * (Dh_m ** 1.24)       # m³/s
 Qp_smpdbk = 1.4 * (Dv_acft ** 0.5) * (Dh_ft ** 1.5)            # ft³/s
 
 # Convert Froehlich Qp from m³/s to ft³/s
-Qp_froehlich_cfs = Qp_froehlich * 35.3147
+Qp_froehlich_cfs = Qp_froehlich * 35.3147 # Original rules of thumb uses 35.31
 
 # Round results
 results = {
     "Method": ["Froehlich", "SMPDBK","Von Thun & Gillette" ],
     "Breach Width (ft)": [round(Bf, 2),round(Bm, 2),round(Bv, 2)],
     "Formation Time (min)": [round(Tf_froehlich, 2), round(Tf_smpdbk, 2), round(Tf_von_thun, 2)],
-    "Peak Outflow (cfs)": [round(Qp_froehlich_cfs, 2), round(Qp_smpdbk, 2),"N/A"]
+    "Peak Outflow (cfs)": [round(Qp_froehlich_cfs, 2), round(Qp_smpdbk, 2),None]
 }
 
 
@@ -190,33 +224,62 @@ for method_name, q_peak in [
         })
 
 downstream_df = pd.DataFrame(results_dstrm)
-#st.dataframe(downstream_df.iloc[-2:],use_container_width=True, hide_index=True) 
 
 df = pd.DataFrame(results)
+#Convert to strings before the apply map in order to control the decimals to 2
+for i in df.columns[1:]:
+    df[i] = df[i].apply(lambda x: '{:.2f}'.format(x))
 
-tab1, tab2 = st.tabs(["Results", "Equation Information"])
+tab1, tab2, tab3 = st.tabs(["Breach Width/Timing, Peak Q", "Travel Time", "Equation Information"])
 
-def highlight_by_damtype(value):
-    if dam_type == "Earthen" and value == "Froehlich":
-        color = f"background-color: lightgreen;"
-    elif dam_type != "Earthen" and value == "SMPDBK":
-        color = f"background-color: lightgreen;"
-    else:
-        color = None
-    return  color#f"border: red;" if dam_type == "Earthen" and value == "Froehlich" else None
-
-with tab1:
-    #st.markdown("#### Results")
+with tab1:    
     st.markdown("##### Breach Width, Formation Time, and Peak Outflow")
     st.caption("Prefered equation highlighted in green based on dam type specified.")
-    st.dataframe(df.style.applymap(highlight_by_damtype), use_container_width=True, hide_index=True)
+    st.dataframe(df.style.map(highlight_by_damtype,subset=["Method"]), use_container_width=True, hide_index=True)
     #Print downstream Q and height df
     st.markdown("##### Downstream Peak Discharge and Height Estimate")
     st.caption("Initial wave height in downstream calculations is assumed to be 40% of Dam Breach Head")
 
     st.dataframe(downstream_df[downstream_df['Mile'] == downstream_mileage],use_container_width=True, hide_index=True)
-#Equation infomation Tabs
+
+    # Prepare Altair-compatible data
+    plot_df = downstream_df.copy()
+    plot_df["Mile"] = round(plot_df["Mile"],2)
+    plot_df['Peak Q Formula'] = plot_df['Method']
+
+    # Peak Discharge Plot
+    st.markdown("#### 📉 Peak Discharge vs. Distance")
+    chart_q = alt.Chart(plot_df).mark_line(point=True).encode(
+        x=alt.X('Mile:O', title='Distance Downstream (miles)'),
+        y=alt.Y('Peak Discharge (cfs):Q'),
+        color='Peak Q Formula:N'
+    ).properties(width=700, height=350)
+    st.altair_chart(chart_q, use_container_width=True)
+
+
 with tab2:
+    st.markdown("#### Flood wave Velocity")
+    st.write("Flood wave velocity varies across the county depending upon the slope and vegetation density, but some estiamtes can be made based on historical dam breaks. Except at the Damsite, average downstream speeds of a flood wave are in the range of:")
+    st.markdown("* 3 to 4 miles per hour for valley areas \n * 5 to 7 miles per hour for foothills\n* 8 to 10 miles per hour for mountainous areas")
+    st.markdown("##### Fread Velocity")
+    manningsn = st.number_input("Manning's n", min_value=0.00, value=0.05, step=0.01)
+    slope = st.number_input("Channel slope (ft/mi)", min_value=5.0, value=10.0, step=0.1)
+
+    fread = np.round((0.005/manningsn) * Dh_ft**(2/3) * slope**(0.5),2)
+    fread_time = np.round(downstream_mileage / fread,2)
+
+    st.write("Using Dam Breach Height(ft): ", Dh_ft)
+    st.write("Fread Velocity (mph): ", fread)
+    st.write("Downstream point distance (mi): ", np.round(downstream_mileage,2))
+    st.write("Time to downstream point (hr): ", fread_time)
+
+    st.markdown("##### OHD Velocity")
+    st.caption("See Equation info. Uses Wave Height of 40% of Dam Breach head.")
+    ohd_v = np.round(3*(Dh_ft*0.4)**0.33)
+    st.write("OHD velocity (mph): ", ohd_v)
+
+#Equation infomation Tabs
+with tab3:
     st.write('Below information is taken directly from LMRFC Dam Break ROT')
     with st.expander("Froehlich"):
         file_content_bp = read_text_file("docs/bpFroehlich.hlp")  
@@ -233,32 +296,12 @@ with tab2:
         st.markdown(file_content_bp)
     with st.expander("Downstream Peak Discharge and Height"):
         file_content_bp = read_text_file("docs/pfDownstream.hlp")  
-        st.markdown(file_content_bp)  
-
-
-
-# Prepare Altair-compatible data
-plot_df = downstream_df.copy()
-plot_df["Mile"] = round(plot_df["Mile"],2)
-plot_df['Peak Q Formula'] = plot_df['Method']
-
-# Peak Discharge Plot
-st.markdown("#### 📉 Peak Discharge vs. Distance")
-chart_q = alt.Chart(plot_df).mark_line(point=True).encode(
-    x=alt.X('Mile:O', title='Distance Downstream (miles)'),
-    y=alt.Y('Peak Discharge (cfs):Q'),
-    color='Peak Q Formula:N'
-).properties(width=700, height=350)
-st.altair_chart(chart_q, use_container_width=True)
-
-# Estimated Depth Plot
-#st.markdown("#### 📈 Estimated Depth vs. Distance")
-chart_h = alt.Chart(plot_df).mark_line(point=True).encode(
-    x=alt.X('Mile:O', title='Distance Downstream (miles)'),
-    y=alt.Y('Estimated Depth (ft):Q'),
-    color='Method:N'
-).properties(width=700, height=350)
-#st.altair_chart(chart_h, use_container_width=True)
+        st.markdown(file_content_bp)
+    with st.expander("Travel Times / Velocity"):
+        file_content_fr = read_text_file("docs/frVelocity.hlp")  
+        st.markdown(file_content_fr)
+        file_content_pf = read_text_file("docs/pfVelocity.hlp")  
+        st.markdown(file_content_pf)  
 
 st.markdown("---")
 st.markdown("This tool is based on empirical methods for estimating dam breach parameters. \n To be updated with equations and assumptions.")
